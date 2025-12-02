1e06fcc44d5898a0d6b2246cd38b4b80f561d76b
Rumasoreng Fadil <limitvariabel@gmail.com>
2025-12-02 16:37:48 +0700

    chore : creare auth controller

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HttpController } from './infrastructure/http/http.controller';
+import { AuthController } from './infrastructure/http/auth/auth.controller';
+import { AuthControllerController } from './infrastructure/http/auth.controller/auth.controller.controller';
+import { AuthController } from './infrastructure/http/auth.controller';
+import { AuthControllerController } from './infrastructure/http/auth.controller/auth.controller.controller';
 
 @Module({
-  controllers: [HttpController]
+  controllers: [HttpController, AuthController, AuthControllerController]
 })
 export class AuthModule {}
