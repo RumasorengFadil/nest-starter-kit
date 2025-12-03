628d41b1cc42af32308adb0c1e433f694cc74365
Rumasoreng Fadil <limitvariabel@gmail.com>
2025-12-03 09:30:53 +0700

    chore : create verify-email and welcome-email templates

---
+++ src/modules/auth/domain/entities/verification-token.entity.ts
@@ -0,0 +1,17 @@
+import { User } from 'src/modules/user/domain/entities/user.entity';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+
+@Entity()
+export class VerificationToken {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  token: string;
+
+  @ManyToOne(() => User, user => user.verificationTokens)
+  user: User;
+
+  @Column()
+  expiresAt: Date;
+}
--- src/modules/user/domain/entities/user.entity.ts
+++ src/modules/user/domain/entities/user.entity.ts
@@ -1,23 +1,27 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { AuthProvider } from '../provider/auth.provider';
+import { VerificationToken } from 'src/modules/auth/domain/entities/verification-token.entity';
 
 @Entity('users')
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ type:'varchar', unique: true, nullable: true })
+  @Column({ type: 'varchar', unique: true, nullable: true })
   email: string | null;
 
-  @Column({type:'varchar',nullable: true })
+  @Column({ type: 'varchar', nullable: true })
   password: string | null; // hashed
 
   @Column({ type: 'enum', enum: AuthProvider, default: AuthProvider.LOCAL })
   provider: AuthProvider;
 
-  @Column({type:'varbinary', nullable: true })
+  @Column({ type: 'varbinary', nullable: true })
   providerId: string | null; // google id
 
-  @Column({type:'varchar', nullable: true })
+  @Column({ type: 'varchar', nullable: true })
   refreshTokenHash: string | null; // hashed refresh token
+
+  @OneToMany(() => VerificationToken, (token) => token.user)
+  verificationTokens: VerificationToken[];
 }
