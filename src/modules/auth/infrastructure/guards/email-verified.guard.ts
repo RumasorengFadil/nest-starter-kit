87c97b61ac87b0832f1f055fc9a07747656a8c80
Rumasoreng Fadil <limitvariabel@gmail.com>
2025-12-04 10:22:15 +0700

    chore : create email verified guard

---
+++ src/modules/auth/infrastructure/guards/email-verified.guard.ts
@@ -0,0 +1,24 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+
+@Injectable()
+export class EmailVerifiedGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (!user) {
+      throw new ForbiddenException('User not found in request');
+    }
+
+    if (!user.isEmailVerified) {
+      throw new ForbiddenException('Please verify your email first');
+    }
+
+    return true;
+  }
+}
