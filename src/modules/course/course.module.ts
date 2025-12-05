6cdaa03e92660424f655c9f01e0715ff3f0769c0
Rumasoreng Fadil <limitvariabel@gmail.com>
2025-12-05 14:42:40 +0700

    chore : create course service

--- src/modules/course/course.module.ts
+++ src/modules/course/course.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { CourseService } from './domain/services/course.service';
 
-@Module({})
+@Module({
+  providers: [CourseService]
+})
 export class CourseModule {}
