7837861c19863541ed860455cd74d536361f5b73
Rumasoreng Fadil <limitvariabel@gmail.com>
2025-12-05 14:50:39 +0700

    chore : create course controller

--- src/modules/course/course.module.ts
+++ src/modules/course/course.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { CourseService } from './domain/services/course.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from './domain/entities/course.entity';
+import { CourseController } from './infrastructure/http/course.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Course])],
   providers: [CourseService],
+  controllers: [CourseController],
 })
 export class CourseModule {}
